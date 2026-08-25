ece0e75273871e19b54a97239b6f5f092cb4e80d
Suprit Pal <palsuprit1409@gmail.com>
2026-08-25 11:44:22 +0530

    update API_URL to local network IP in app-frontend

--- app-frontend/config.js
+++ app-frontend/config.js
@@ -1,2 +1,2 @@
-// export const API_URL = 'https://ecosyz-backend.onrender.com/api';
-export const API_URL = 'http://localhost:8000/api';
+// export const API_URL = 'http://localhost:8000/api';
+export const API_URL = 'http://192.168.0.104:8000/api';
